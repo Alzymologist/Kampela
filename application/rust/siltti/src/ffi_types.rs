0946875afb704d748135ce5d96df404eea369aa2
Vera Abramova <abramova.vera@gmail.com>
2022-09-30 14:43:44 +0300

    feat: companion signing for all transmittables

--- application/rust/siltti/src/ffi_types.rs
+++ application/rust/siltti/src/ffi_types.rs
@@ -1,2 +1,6 @@
-pub use database::{error::ErrorCompanion, process_input::Action, sign_with_companion::{SignedByCompanion, SignedData}};
+pub use database::{
+    error::ErrorCompanion,
+    process_input::Action,
+    sign_with_companion::{SignedByCompanion, SignedData},
+};
 pub use qr_reader::{Collection, ErrorQr, Frames, Payload};
