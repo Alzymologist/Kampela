79876a3c56918ef0794237217489879f2f65ea0b
Vera Abramova <abramova.vera@gmail.com>
2022-11-13 20:21:35 +0200

    chore: forgot fmt

--- application/rust/database/src/tests.rs
+++ application/rust/database/src/tests.rs
@@ -3,7 +3,8 @@ use crate::storage::{MetadataStorage, SpecsValue};
 
 #[test]
 fn good_specs_processing_1() {
-    let payload_hex = std::fs::read_to_string("for_tests/add_specs_rococo-sr25519_Alice-sr25519.txt").unwrap();
+    let payload_hex =
+        std::fs::read_to_string("for_tests/add_specs_rococo-sr25519_Alice-sr25519.txt").unwrap();
     let payload = hex::decode(payload_hex).unwrap();
     let specs_result = SpecsValue::from_payload_prelude_cut(&payload[3..], &Encryption::Sr25519);
     assert!(specs_result.is_ok());
@@ -11,8 +12,10 @@ fn good_specs_processing_1() {
 
 #[test]
 fn good_meta_processing() {
-    let payload_hex = std::fs::read_to_string("for_tests/load_metadata_westendV9122_Alice-sr25519.txt").unwrap();
+    let payload_hex =
+        std::fs::read_to_string("for_tests/load_metadata_westendV9122_Alice-sr25519.txt").unwrap();
     let payload = hex::decode(payload_hex).unwrap();
-    let meta_result = MetadataStorage::from_payload_prelude_cut(&payload[3..], &Encryption::Sr25519);
+    let meta_result =
+        MetadataStorage::from_payload_prelude_cut(&payload[3..], &Encryption::Sr25519);
     assert!(meta_result.is_ok());
 }
