a0f8e56119c2d9955af2463fff30a2834129f93d
SmallSharky <kizzyol76@gmail.com>
2023-08-16 19:10:24 +0300

    Add Readme, upgrade binarize.sh to use pilkki flasher

--- firmware/development/kampela/binarize.sh
+++ firmware/development/kampela/binarize.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+set -e
+
+echo "Building..."
+
 cargo build --release
-mv target/thumbv8m.main-none-eabihf/release/app target/thumbv8m.main-none-eabihf/release/app.elf
-arm-none-eabi-objcopy -O binary target/thumbv8m.main-none-eabihf/release/app.elf target/thumbv8m.main-none-eabihf/release/app.bin
-commander flash target/thumbv8m.main-none-eabihf/release/app.bin --device EFM32PG23B200F512IM40 --address 0x08000000
+
+BUILD_DIRECTORY="target/thumbv8m.main-none-eabihf"
+EXECUTABLE="$BUILD_DIRECTORY/release/app"
+FIRMWARE="$BUILD_DIRECTORY/release/app.bin"
+
+echo "Preparing binary file..."
+
+arm-none-eabi-objcopy -O binary "$EXECUTABLE" "$FIRMWARE"
+
+echo "Flashing..."
+
+if [[ -z "$1" || "$1" == "--pilkki" ]]; then
+pilkki write -i "$FIRMWARE"
+elif [ "$1" == "--segger" ]; then
+commander flash "$FIRMWARE" --device EFM32PG23B200F512IM40 --address 0x08000000
+else
+echo "What means \"$1\"? Usage:"
+echo "$0 [--pilkki|--segger]"
+exit -1
+fi
